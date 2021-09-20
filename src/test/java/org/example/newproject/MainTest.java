93cf6f520d77bd1bd28ae36c5f112a9538adb548
Anvaqta Tangguh Wisesa <anvaqta@student.telkomuniversity.ac.id>
2021-09-20 21:43:32 +0700

    add example class & test files

---
+++ src/test/java/org/example/newproject/MainTest.java
@@ -0,0 +1,13 @@
+package org.example.newproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void exampleTestWhichIsDoNothing(){
+        assertTrue(true);
+    }
+
+}
